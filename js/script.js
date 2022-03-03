c57cd280eed5ea6f66ce2961c0f95263dd1299d3
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-03-03 15:06:22 +0100

    product in cart added and method toggle in js added, cart open/close  work well

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+ 
 const menu = document.querySelector(".menu")
 const hamburgerBtn = document.querySelector(".hamburger")
 const closeBtn = document.getElementById("close-menu")
+const btnCart = document.querySelector(".btnCart")
+const cart = document.querySelector(".cart")
 
+
+// Hamburger Menu
 hamburgerBtn.addEventListener("click", onHamburgerClick)
 closeBtn.addEventListener("click", onCloseClick)
 
@@ -11,4 +16,13 @@ function onHamburgerClick() {
 
 function onCloseClick() {
     menu.classList.add("hidden")
+}
+
+// Product in cart - when cart is closed .toggle() method make it open.
+// when cart is open .toggle() method makes it closed.
+
+btnCart.addEventListener("click", openCartWhenClicked)
+
+function openCartWhenClicked() {
+    cart.classList.toggle("hidden")
 }
