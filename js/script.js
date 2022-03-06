e99fa89396518240a04646bd2a42971dd22b46bb
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2022-03-06 20:10:42 +0100

    increment and decrement product feature added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const hamburgerBtn = document.querySelector(".hamburger")
 const closeBtn = document.getElementById("close-menu")
 const btnCart = document.querySelector(".btnCart")
 const cart = document.querySelector(".cart")
+const btnMinus = document.getElementById("btnMinus")
+const btnPlus = document.getElementById("btnPlus")
+const productCounter = document.querySelector(".counter")
+
+let productCounterValue = 1
+
 
 
 // Hamburger Menu
@@ -12,6 +18,10 @@ closeBtn.addEventListener("click", onCloseClick)
 
 // Product in cart
 btnCart.addEventListener("click", openCartWhenClicked)
+
+//Increment or decrement the counter
+btnPlus.addEventListener("click", productCounterIncrement)
+btnMinus.addEventListener("click", productCounterDecrement)
 
 function onHamburgerClick() {
     menu.classList.remove("hidden")
@@ -26,4 +36,19 @@ function onCloseClick() {
 
 function openCartWhenClicked() {
     cart.classList.toggle("hidden")
+}
+
+function productCounterIncrement() {
+    setProductCounterValue(1)
+}
+
+function productCounterDecrement() {
+    setProductCounterValue(-1)
+}
+
+function setProductCounterValue(value) {
+    if ((productCounterValue + value) > 0) {
+        productCounterValue += value
+        productCounter.textContent = productCounterValue
+    }
 }
